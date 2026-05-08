5936953edb6c928451fb939570657df23ad6dca3
unknown <you@example.com>
2026-05-08 11:49:06 +0600

    update profile

---
+++ src/components/UpdateUserModal.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Button, Input, Label, Modal, Surface, TextField } from "@heroui/react";
+import { useState } from "react";
+import { BiEdit, BiUser } from "react-icons/bi";
+import { useRouter } from "next/navigation"; 
+
+export function UpdateUserModal() {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter(); 
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const image = e.target.image.value;
+    try {
+      await authClient.updateUser({
+        name: name,
+        image: image,
+      });
+      
+      setIsOpen(false); 
+      router.push("/profile"); 
+      router.refresh(); 
+
+    }
+     catch (error) {
+      console.error("Update failed", error);
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={setIsOpen}>
+      <Button onPress={() => setIsOpen(true)} variant="secondary">
+        <BiEdit /> Update Profile
+      </Button>
+
+      <Modal.Backdrop>
+        <Modal.Container placement="auto">
+          <Modal.Dialog className="sm:max-w-md">
+            <Modal.CloseTrigger />
+            <Modal.Header>
+              <Modal.Icon className="bg-accent-soft text-accent-soft-foreground">
+                <BiUser className="size-5" />
+              </Modal.Icon>
+              <Modal.Heading>Update User</Modal.Heading>
+            </Modal.Header>
+            <Modal.Body className="p-6">
+              <Surface variant="default">
+                <form onSubmit={onSubmit} className="flex flex-col gap-4">
+                  <TextField className="w-full" name="name" type="text">
+                    <Label>Name</Label>
+                    <Input placeholder="Enter your name" />
+                  </TextField>
+                  <TextField className="w-full" name="image" type="url">
+                    <Label>Image URL</Label>
+                    <Input placeholder="Image url" />
+                  </TextField>
+
+                  <Modal.Footer>
+                    <Button onPress={() => setIsOpen(false)} variant="secondary">
+                      Cancel
+                    </Button>
+                    <Button type="submit" color="primary">
+                      Save
+                    </Button>
+                  </Modal.Footer>
+                </form>
+              </Surface>
+            </Modal.Body>
+          </Modal.Dialog>
+        </Modal.Container>
+      </Modal.Backdrop>
+    </Modal>
+  );
+}
